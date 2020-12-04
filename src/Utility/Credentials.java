50ff183def056916a132c22618f0887206e1a2a2
Med Aziz Njaimi <mohamedaziz.njaimi@esprit.tn>
2020-12-04 21:26:32 +0100

    Structure

---
+++ src/Utility/Credentials.java
@@ -0,0 +1,19 @@
+package Utulitaire;
+
+public interface Credentials {
+    //Email Credentials 
+    String from = "bookstore.cm.esprit@gmail.com";
+    String password = "bookstore1234";
+    // Nexmo Api Credentials 
+    String nexmo_apiKey = "b17813bb";//4703c017
+    String nexmo_apiSecret = "D6YjaxTfSEBiIuph";//56AQOK9KrbQbPqIY
+
+    String host ="brbrcv3yusbod6uggq90-mysql.services.clever-cloud.com";
+    String dbname = "brbrcv3yusbod6uggq90";
+    String url = "jdbc:mysql://"+host+":3306/"+dbname;
+    String user = "ufwcrzhlecbvqmn5";
+    String passwd = "lFBBoU6swhBLissd766v";
+    String driver = "com.mysql.jdbc.Driver";
+    String sgbd_name = "MySQL";
+
+}
