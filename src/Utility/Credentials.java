c9e3fc4a9ab651cd75ed15977985d05f21901b37
Hamdi-Limam <limamhamdi13@gmail.com>
2020-12-10 22:36:08 +0100

    Online Library Final Version

---
+++ src/Utility/Credentials.java
@@ -0,0 +1,19 @@
+package Utulitaire;
+
+public interface Credentials {
+    //Email Credentials 
+    String from = "bookstore.cm.esprit@gmail.com";
+    String password = "bookstore**";
+    // Nexmo Api Credentials 
+    String nexmo_apiKey = "b17813**";
+    String nexmo_apiSecret = "D6YjaxTfSEBiIu**";
+
+    String host ="brbrcv3yusbod6uggq**-mysql.services.clever-cloud.com";
+    String dbname = "brbrcv3yusbod6uggq**";
+    String url = "jdbc:mysql://"+host+":3306/"+dbname;
+    String user = "ufwcrzhlecbvqmn5";
+    String passwd = "lFBBoU6swhBLissd766v";
+    String driver = "com.mysql.jdbc.Driver";
+    String sgbd_name = "MySQL";
+
+}
